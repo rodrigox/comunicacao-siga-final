8eb1c437bc5ec27811aa961c3c08d7e054e3f5ff
rodrigox <rdrigoalex@gmail.com>
2018-01-16 23:41:42 -0300

    primeiro commit all files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,27 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { RodapeComponent } from './rodape/rodape.component';
+import { BannerComponent } from './banner/banner.component';
+import { BodyComponent } from './body/body.component';
+import { ContatoComponent } from './contato/contato.component';
+import { BodyContentComponent } from './body/body-content/body-content.component';
+import { BodyContentContatoComponent } from './body/body-content/body-content-contato/body-content-contato.component';
+import { ImageGalleryComponent } from './image-gallery/image-gallery.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    RodapeComponent,
+    BannerComponent,
+    BodyComponent,
+    ContatoComponent,
+    BodyContentComponent,
+    BodyContentContatoComponent,
+    ImageGalleryComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/body/body-content/body-content-contato/body-content-contato.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-body-content-contato',
+  templateUrl: './body-content-contato.component.html',
+  styleUrls: ['./body-content-contato.component.css']
+})
+export class BodyContentContatoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
